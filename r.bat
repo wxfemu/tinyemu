249b9c5eb36615e2e48ec5f3b91dfe22de7a5da3
WXF <35006028+wxuefei@users.noreply.github.com>
2024-12-23 11:49:16 +0800

    Create r.bat

---
+++ r.bat
@@ -0,0 +1,7 @@
+#./temu -ctrlc https://bellard.org/jslinux/buildroot-riscv64.cfg
+
+#./temu -ctrlc http://127.0.0.1/jslinux/buildroot-riscv64.cfg
+#./temu -ctrlc http://127.0.0.1/jslinux/buildroot-riscv64-xwin.cfg
+./temu -ctrlc http://127.0.0.1/jslinux/buildroot-riscv64-tty.cfg
+
+stty echo
